67230111034ce69bbf458ee798326ed23f4ff107
JacobGriffithElliott <j.o.gelliott@gmail.com>
2021-07-19 15:44:24 -0700

    MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Link, Route, Switch } from 'react-router-dom';
+import Movie from './Movies/Movie.js';
+import MovieList from './Movies/MovieList.js';
 import SavedList from './Movies/SavedList';
 
 export default function App () {
@@ -14,6 +16,7 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -29,8 +32,14 @@ export default function App () {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
-
-      <div>Replace this Div with your Routes</div>
+        <Switch>
+          <Route exact path='/'>
+            <MovieList movies={movieList}/>
+          </Route>
+          <Route path='/movies/:id'>
+            <Movie movie={movieList} />
+          </Route>
+        </Switch>
     </div>
   );
 }
